04f4cab94516c64c6e37af5d341ff37d682f33a8
George Pittas <fozuzip@gmail.com>
2023-08-14 19:48:17 +0300

    Add prisma Category model and seed

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -17,6 +17,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
+      <div className="sr-only" aria-hidden="true"></div>
       <DropdownMenuTrigger asChild>
         <Button variant="secondary" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
